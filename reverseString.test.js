73626ec61aeb7d6ebc9e82181c5d5933b0709eca
Pierre <piercel.coder2022@gmail.com>
2022-07-23 13:58:08 +0200

    Revert "Workflow for testing"
    This reverts commit f711f3783ab1b496d023289ec2b571e01fbf1850.

--- reverseString.test.js
+++
@@ -1,5 +0,0 @@
-const reverseString = require('./reverseString');
-
-test('the reverse of hello is olleh', () => {
-    expect(reverseString("hello")).toBe('olleh');
-});
--- stringLength.js
+++
@@ -1,8 +0,0 @@
-
-function stringLength (string) {  
-  if (string.length < 1 || string.length > 10){
-    return false;
-  }
-  return string.length;
-  }
-module.exports = stringLength;
